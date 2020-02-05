f62af63dc328f1b0fcc9c7e5ecd984e2df93d520
Bijay Shrestha <bijay.shrestha@f1soft.com>
2020-02-05 16:22:19 +0545

    First Commit

---
+++ src/main/java/com/bijay/miniostorage/MinioStorageApplication.java
@@ -0,0 +1,13 @@
+package com.bijay.miniostorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinioStorageApplication.class, args);
+	}
+
+}
